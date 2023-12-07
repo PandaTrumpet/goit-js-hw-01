ff9c8f102e3635390b3eaa1e12392c4522671431
PandaTrumpet <dsavontrumpet@gmail.com>
2023-12-07 11:31:06 +0200

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
+'use strict'
 function getShippingMessage(country, price, deliveryFree) {
     let totalPrice = price + deliveryFree;
     return `Shipping to ${country} will cost ${totalPrice} credits`
 };
-
 
 console.log(getShippingMessage("Australia", 120, 50));
 console.log(getShippingMessage("Germany", 80, 20));
